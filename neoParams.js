3299c72639301e43e4210341f09189552885908f
shaikatzir <katzir.shai@gmail.com>
2013-09-20 09:33:30 +0300

    added parameters

--- neoParams.js
+++ neoParams.js
@@ -1,52 +1,26 @@
 
-DEFAULT_MODELS = ['51ab7e778638fa9f2100002a']
 
 //Graph DB colors
 COLORS = ['Black','White','Grey','Blue','Red','Beige','Pink','Brown','Yellow','Orange','Green','Purple','Leopard', 'Zebra','Gold','Silver']
 
-COLORS_A = ['NAVY','ECRU','MARENGO','CAMEL','SAND','462','DARK KHAKI','INK BLUE','TURQUOISE','ICE','MAROON','PALE MARL','DARK MARL','STONE MARL','CHOCOLATE','MUSTARD','0-341','VANILLA', 'CREAM', 'NEUTRAL','BORDEAUX','BURDEOS','BURGUNDY','CARMINE','CARAMEL','WINE','FUCHSIA','GOLD','SILVER', 'STONE','STRAWBERRY','SHRIMP','IVORY','ROYAL','YELLOW','CORAL','JEANS','GRAY','NUDE','BANANA','GRAY','PEACH','MOCHA','FUSCHIA','ROSA','LIME','JEANS','BEIGE', 'MINT', 'BLUISH','MYSTERY','WATER','GERANIUM','PEARL','APPLE','FUCSIA','MENTOL','DENIM']
 
-COLORS_C = ['NAVY','WHITE','GREY','CAMEL','BEIGE','BLUE','KHAKI','BLACK','TURQUOISE','WHITE','RED','GREY','GREY','WHITE','BROWN','YELLOW','GREY','WHITE','WHITE','WHITE','BORDEAUX','BORDEAUX','BORDEAUX','BORDEAUX','BROWN','RED','FUCHSIA','GOLD','SILVER','BEIGE','PINK','PINK','WHITE','ROYAL','BANANA','CORAL','JEANS','GREY','BEIGE','BANANA','GREY','PEACH','BROWN','FUSCHIA','PINK','LIME','BLUE','CHAMPAGNE', 'GREEN', 'BLUE','BEIGE','BLUE','PINK','BEIGE','YELLOW','PINK','LIGHT GREEN','JEANS']
-
-COLORS_B = ['BLUE','WHITE','GREY','BEIGE','BEIGE','BLUE','BROWN','BLACK','BLUE','WHITE','RED','GREY','GREY','WHITE','BROWN','YELLOW','GREY','WHITE','WHITE','WHITE','RED','RED','RED','RED','BROWN','RED','PINK','GOLD','SILVER','BEIGE','PINK','PINK','WHITE','BLUE','YELLOW','PINK','BLUE','GREY','BEIGE','YELLOW','GREY','ORANGE','BROWN','PINK','PINK','YELLOW','BLUE','CHAMPAGNE', 'GREEN',' BLUE','BEIGE','BLUE','PINK','BEIGE','YELLOW','PINK','GREEN','BLUE']
-
-PATTERNS_distinct = ['LEOPARD', 'ZEBRA', 'ANIMAL', 'FLORAL', 'STRIPE', 'DOT', 'GEOMATRIC PRINT', 'PRINT', 'CAMOUFLAGE', 'PLAID', 'SNAKE', 'TWO COLOR', 'NONE']
-
-PATTERNS_dic = ['LEOPARD', 'ZEBRA', 'ANIMAL', 'FLORAL','FLOREALE', 'STRIPE','POLKA','DOT','SPOTTED','GEOMATRIC PRINT','PRINT', 'CAMOUFLAGE', 'PLAID', 'SNAKE','TWO TONE', 'TWO COLOR','TWO-COLOR','TWO-TONE', 'PATTERN', 'DRAPING','CHECKED', 'NONE']
 //Graph DB patterns
-PATTERNS = ['Leopard', 'Zebra', 'Animal', 'Floral','Dot','Plain','Print', 'Stripe', 'Geomatric Print', 'Camouflage', 'Plaid', 'Snake','Two Color', 'None']
-PATTERN_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0]
-
-MATERIALS_dic = ['LACE', 'LEATHER', 'SUEDE', 'FUR', 'SILK', 'STITCH','NET','STRING', 'NYLON', 'WOOL', 'SATIN', 'CHIFFON','JEANS','SEQUIN','SHEAR', 'DENIM','VELVET','ANGORA','CROCHET','TRANSPARENT']
+PATTERNS = ['Leopard', 'Zebra', 'Animal', 'Floral','Dot','Plain','Print', 'Stripe', 'Geomatric Print', 'Camouflage', 'Plaid', 'Snake','Two Color']
 //Graph DB Materials
 MATERIALS = ['Chiffon','Lace', 'Leather', 'Suede', 'Fur', 'Silk', 'Stitch', 'Nylon', 'Wool', 'Satin', 'Sequin','Shear', 'Jeans', 'Velvet','Transparent']
-MATERIALS_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16]
 
 
-//sub categories
-item_types_A = ['top','jersey','knit','tank', 'tunic','coat','skirt', 'dress', 'shirt', 'blouse','blazer','jacket','jumper','sweater','shorts' ,'trousers', 'jeans', 'bag', 'cardigan', 'shoes', 'accessories', 'sweatshirt', 'scarve', 'scarf', 'jewellery','vest','sweater', 'vest','jumpsuit','poncho', 'earrings', 'necklace', 'bracelet', 'ring','pants', 'bolero','cape', 'sandal', 'leggings', 'legging', 'clutch', 'boots', 'parka', 'trench', 'jeggings', 'backpack', 'shoper','purse', 'wedges', 'loafers', 'ballerinas','overall', 'anorak','swimwear','sunglass','bikini bottoms','bikini top','bikini','slingbacks','wristwear','neckwear','hat','chinos']
-//matching categories for the sub-categories above (item_types_A[i] is a sub category of item_types_B[i])
-item_types_B = ['Top','Top','Top','Top','Top','Coat', 'Skirt', 'Dress', 'Top','Top', 'Blazer', 'Blazer', 'Sweater','Sweater','Shorts','Trousers', 'Jeans', 'Handbag', 'Cardigan', 'Shoes', 'Accessories', 'Sweatshirt', 'Scarf', 'Scarf','Jewellery','Vest','Sweater', 'Vest','Jumpsuit', 'Poncho', 'Earrings', 'Necklace', 'Bracelet', 'Ring','Trousers', 'Cardigan','Poncho', 'Sandal', 'Leggings', 'Leggings', 'Handbag', 'Boots','Coat', 'Coat', 'Jeans', 'Backpack', 'Handbag', 'Handbag', 'Wedges', 'Shoes', 'Shoes','Jumpsuit', 'Coat','Swimwear','Sunglass','Bikini Bottoms','Bikini Top','Bikini','Shoes','Bracelet','Necklace', 'Hat','trousers']
-
-//Graph DB types
-item_types = ['Top','Coat', 'Skirt', 'Dress', 'Blazer','Sweater','Shorts','Trousers', 'Jeans', 'Handbag', 'Cardigan', 'Shoes', 'Accessories', 'Sweatshirt', 'Scarf', 'Scarf','Jewellery','Vest','Sweater', 'Vest','Jumpsuit', 'Poncho', 'Earrings', 'Necklace', 'Bracelet', 'Ring','Trousers', 'Cardigan','Poncho', 'Sandal', 'Leggings', 'Leggings', 'Handbag', 'Boots','Coat', 'Coat', 'Jeans', 'Backpack', 'Handbag', 'Handbag', 'Wedges', 'Shoes', 'Shoes','Jumpsuit', 'Coat','Swimwear','Sunglass','Bikini Bottoms','Bikini Top','Bikini','Shoes','Bracelet','Necklace', 'Hat','trousers']
-
-categories_A = ['SLEEVELESS', 'STRAPLESS', '3/4-SLEEVE','THREE-QUARTER-LENGTH-SLEEVE', '3/4-LENGTH-SLEEVE', 'ONE-SHOULDER','OPEN-SHOULDER', 'OFF-SHOULDER', 'SHORT-SLEEVE', 'SPAGHETTI-STRAP', 'STRAP', 'LONG-SLEEVE','ASYMMETRIC-SLEEVE', '3/4-LENGTH SLEEVES','NONE']
 //Graph DB sleeves
-SLEEVES =       ['Sleeveless', 'Strapless', '3/4', 'One-Shoulder', 'Off-Shoulder', 'Short', 'Strap',  'Long', 'Asymmetric','None']
-categories_A_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0]
-categories_B = ['LONG', 'SHORT', 'MIDI', 'ASYMMETRIC','MINI','MAXI','KNEE LENGTH','MIDI', 'NONE']
+SLEEVES =       ['Sleeveless', 'Strapless', '3/4', 'One-Shoulder', 'Off-Shoulder', 'Short', 'Strap',  'Long', 'Asymmetric']
+
 //Graph DB Lengths
 LENGTH = ['Long','Short','Midi','Maxi','Asymmetric','Mini']
-categories_B_num = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-categories_C = ['KEYHOLE NECK', 'FALLING-NECK','WIDE-NECK', 'JEWELLED-COLLAR', 'JEWLLED-NECK', 'V-NECK','V-COLLAR', 'GOLF', 'HIGH-NECK', 'HIGH-COLLAR', 'BUTTON-DOWN', 'BUTTON-NECK', 'TURTLE', 'BOAT-NECK', 'ROUND-NECK','ROUND-NECK','FUR-NECK', 'FUR-DETAIL', 'FUR-COLLAR','FUR-LINED-COLLAR','SHEAR-NECK', 'LACE-NECK', 'LACE-COLLAR', 'ASYMMETRIC-NECK','FUR-PARKA', 'SQUARE-NECK','DOUBLE-COLLAR', 'DOUBLE-NECK', 'COLLAR','HEART-NECK','HALTER-NECK','NONE']
 
 //Graph DB neck
 neck =         ['V','Round','Boat','Halter','Keyhole', 'Falling', 'Wide', 'Jewlled', 'Golf', 'High', 'Button-Down', 'Button', 'Turtle',
-'Fur','Shear', 'Lace', 'Asymmetric', 'Square','Double', 'Collar','Heart','None']
+'Fur','Shear', 'Lace', 'Asymmetric', 'Square','Double', 'Collar','Heart']
 
 
-categories_C_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0]
 //Graph DB misc
 MISC = ['Buttom','Combined Fabric','Flower','Zip','Belt','Wrap', 'Stud', 'Hood', 'Tulle','Back Zip', 'Feather', 'Ruffle','Puff','Sequin', 'Asymmetric Zip','Pocket', 'Pleat', 'Rivets', 'Peplum', 'Tear', 'Bow', 'Tie', 'Patch','On Shoulders','Open Shoulders', 'Fur Hood','Button']
 
